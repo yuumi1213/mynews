194584d97995379d6389e106bc220ff682a937a0
yuumi <941213k@gmail.com>
2020-02-28 08:42:14 +0000

    kadai

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -30,22 +30,6 @@ class ProfileController extends Controller
     }
 }
 
-/*「http://XXXXXX.jp/XXX というアクセスが来たときに、 
-   AAAControllerのbbbというAction に渡す
-   Routingの設定」を書いてみてください。*/
 
-Route::get('XXX', 'Admin\AAAController@bbb');
-
-/*【応用】 前章でAdmin/ProfileControllerを作成し、
-add Action, edit Actionを追加しました。web.phpを編集して、
-admin/profile/create にアクセスしたら ProfileController の add Action に、
-admin/profile/edit にアクセスしたら ProfileController の edit Action に
-割り当てるように設定してください。*/
-
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('/profile/create', 'Admin\ProfileController@add');
-    Route::get('/profile/edit', 'Admin\ProfileController@edit');
-
-});
 
 ?>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,21 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+});
+
+
+
+/*【応用】 前章でAdmin/ProfileControllerを作成し、
+add Action, edit Actionを追加しました。web.phpを編集して、
+admin/profile/create にアクセスしたら ProfileController の add Action に、
+admin/profile/edit にアクセスしたら ProfileController の edit Action に
+割り当てるように設定してください。*/
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('/profile/create', 'Admin\ProfileController@add');
+    Route::get('/profile/edit', 'Admin\ProfileController@edit');
+
+});

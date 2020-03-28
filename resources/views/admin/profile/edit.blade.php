b8863ad6f063c618be373b414235af70cf1be561
yuumi <941213k@gmail.com>
2020-03-28 05:19:58 +0000

    3/28

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -47,16 +47,6 @@
                     <input type="submit" class="btn btn-primary" value="更新">
                 </form>
 <div class="row mt-5">
-                    <div class="col-md-4 mx-auto">
-                        <h2>更新履歴</h2>
-                        <ul class="list-group">
-                            @if ($profile_form->histories != NULL)
-                                @foreach ($profile_form->histories as $history)
-                                    <li class="list-group-item">{{ $history->edited_at }}</li>
-                                @endforeach
-                            @endif
-                        </ul>
-                    </div>
                 </div>
             </div>
         </div>
